1c4abb996081c731f962678e5fe60ea59fb5d1c8
davidlira1 <liradavid100@gmail.com>
2020-03-09 10:23:39 -0700

    Added 3 api routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+//const queries = require('./../db/queries');
 
+
+app.use('/productDescription/:id', (req, res) => {
+    var { id } = req.params;
+
+    // queries.getProductDesc()
+    // .then(productDesc => {
+    //     res.end(productDesc);
+    // })
+    
+})
+
+app.use('/productBenefits/:id', (req, res) => {
+    var { id } = req.params;
+
+    // queries.getProductBenefits()
+    // .then(productBenefits => {
+    //     res.end(productBenefits);
+    // })
+    
+})
+
+app.use('/productDetails/:id', (req, res) => {
+    var { id } = req.params;
+
+    // queries.getProductDetails()
+    // .then(productDetails => {
+    //     res.end(productDetails);
+    // })
+    
+})
 
 
 
