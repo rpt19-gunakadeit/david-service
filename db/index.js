2df1662487fe5658e202f9b2571e24c9ce01bac0
davidlira1 <liradavid100@gmail.com>
2020-04-25 02:47:28 -0700

    made changes to db file, root and password

--- db/index.js
+++ db/index.js
@@ -1,12 +1,9 @@
 const mysql = require('mysql');
 const Promise = require('bluebird');
-// const password = require('./db_password.js');
-// const database = 'fec';
-
 
 var db = mysql.createConnection({
-    user: 'nodeuser',
-    password: 'nodeuser@1234',
+    user: 'root',
+    password: 'pw',
     database: 'nike'
 })
 
